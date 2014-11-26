cbdfab6469f61578a87938ce9c71423b021e75a7
sabres207 <sagesdoron@gmail.com>
2014-11-26 04:11:47 +0200

    first running AngularJS application, generated by yo angular command

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularMailGrabberApp
+ * @description
+ * # angularMailGrabberApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularMailGrabberApp', [
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularMailGrabberApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularMailGrabberApp
+ */
+angular.module('angularMailGrabberApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
