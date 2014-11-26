ab8d5302efe5dd99481f44386e88de0cc750282e
sabres207 <sagesdoron@gmail.com>
2014-11-26 08:34:24 +0200

    first version of the site. right now you can insert an URL address or a text, and the site can take out all the emails that inside them, and list it for you with its count. it uses $.ajax and a site calls whateverorigin.org right now for dilling with all the cross domain work. I don't like it, and wanna see how can i do the same only with angular, or at least, without that site.
    so, in general, need to add:
    1) get the page content somehow only using angularjs utilities.
    2) make the current functionality look normal.
    3) when trying to extract from a URL, show the user something is going on on the background.
    4) add an option to extract to an XL file.
    5) add some kind of 'about me' view.
    6) make the site available online, or figure out how could people use it on the computer without downloading node.js and stuff like that.
    7) make it available on phones via phonegap.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,39 @@
  * Controller of the angularMailGrabberApp
  */
 angular.module('angularMailGrabberApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', '$http', function ($scope) {
+        var holder = 'please type either text or url to extract emails from';
+        $scope.emailsExtracted = [];
+        $scope.holder = holder;
+        $scope.changeHolderState = function(bool) {
+            if(bool) {
+                $scope.holder = '';
+            } else if((typeof $scope.area === 'undefined') ||
+                (typeof $scope.area.text === 'undefined') ||
+                ($scope.area.text.length === 0)) {
+                $scope.holder = holder;
+            }
+        };
+        $scope.extractMails = function(string, bool) {
+            var mailRegex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/gi;
+            if(bool) {
+                $.ajaxSetup({
+                    scriptCharset: "utf-8",
+                    contentType: "application/json; charset=utf-8"
+                });
+
+                $.getJSON('http://whateverorigin.org/get?url=' +
+                        encodeURIComponent(string) +
+                        '&callback=?',
+                    function(data) {
+                        $scope.emailsExtracted = _.uniq(data.contents.match(mailRegex));
+                        $scope.$apply();
+                    })
+                    .error(function() {
+                        alert('sorry man');
+                    });
+            } else {
+                $scope.emailsExtracted = _.uniq(string.match(mailRegex));
+            }
+        };
+  }]);
